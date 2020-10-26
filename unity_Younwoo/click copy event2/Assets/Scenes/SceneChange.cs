e3cbadde6c0dcf78ba0bb1fecbfc125bbde0334d
RohYounwoo <ywroh0619@naver.com>
2020-10-26 22:38:49 +0900

    click copy

---
+++ unity_Younwoo/click copy event2/Assets/Scenes/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChange : MonoBehaviour
+{
+    GameObject child;
+
+    public void setText(string tmpS)
+    {
+        TextMesh playerMetaData = GameObject.Find("playerText").GetComponent<TextMesh>();
+        playerMetaData.text = tmpS;
+    }
+
+    public void ChangeThirdScene()
+    {
+        child = transform.GetChild(0).gameObject;
+        Text playerMetaData = child.GetComponent<Text>();
+        //Text playerMetaData = GameObject.Find("Player1Text").GetComponent<Text>();
+        setText(playerMetaData.text);
+    }
+
+}
