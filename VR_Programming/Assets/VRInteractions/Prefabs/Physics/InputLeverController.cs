9a2237e8327fa949eed82b1ce6f5158e5e53c677
Hoon1029 <hoonisone@gmail.com>
2020-11-02 17:23:56 +0900

    implement lever

---
+++ VR_Programming/Assets/VRInteractions/Prefabs/Physics/InputLeverController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputLeverController : MonoBehaviour
+{
+
+    public string name;
+
+    Camera _mainCam;
+    bool isUP = true;
+    GameObject gameObject;
+    GameObject upLever;
+    GameObject downLever;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCam = Camera.main;
+        upLever = transform.Find("Handle(up)").gameObject;
+        downLever = transform.Find("Handle(down)").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+        {
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if( Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                if(hit.transform.gameObject.name == name)
+                {
+                    clicked();
+                }
+                /*hit.transform.GetComponent<InputLeverController>().clicked();
+                if(Object.ReferenceEquals(hit.transform.gameObject.name, this))
+                {
+                    clicked();
+                }*/
+            }
+        }
+    }
+    void clicked()
+    {
+        if (isUP)
+        {
+            isUP = false;
+            upLever.SetActive(false);
+            downLever.SetActive(true);
+        }
+        else
+        {
+            isUP = true;
+            upLever.SetActive(true);
+            downLever.SetActive(false);
+        }
+    }
+}
---
+++ VR_Programming/Assets/VRInteractions/Prefabs/Physics/OutputLeverController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputLeverController : MonoBehaviour
+{
+    bool isUP = true;
+    GameObject upLever;
+    GameObject downLever;
+    // Start is called before the first frame update
+    void Start()
+    {
+        upLever = transform.Find("Handle(up)").gameObject;
+        downLever = transform.Find("Handle(down)").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+        {
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(ray, out hit);
+
+
+
+            if (hit.collider != null)
+            {
+                clicked();
+            }
+        }
+    }
+
+    void clicked()
+    {
+        if (isUP)
+        {
+            isUP = false;
+            upLever.SetActive(false);
+            downLever.SetActive(true);
+        }
+        else
+        {
+            isUP = true;
+            upLever.SetActive(true);
+            downLever.SetActive(false);
+        }
+    }
+}
