c8e05fc64a39f368b91efa2b655df4135f8136bc
RohYounwoo <ywroh0619@naver.com>
2020-11-20 04:13:25 +0900

    db

---
+++ unity_Younwoo/usingDataBase/Assets/Scenes/ScriptInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptInfo
+{
+    public string[] scriptCode;
+    private static int scriptLength;
+
+    public void setScriptCode(string[] sc, int len)
+    {
+        scriptCode = sc;
+        scriptLength = len;
+    }
+
+    public string[] GetScriptCode()
+    {
+        return scriptCode;
+    }
+
+    public int ScriptLength ()
+    {
+        return scriptLength;
+    }
+
+}
---
+++ unity_Younwoo/usingDataBase/Assets/Scenes/getS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //https://www.youtube.com/watch?v=4W90-mh70JY
+
+    public IEnumerator Start()
+    {
+        
+        
+        
+        WWWForm form = new WWWForm();
+        form.AddField("level", 1);
+        form.AddField("stage", 1);
+
+
+        WWW request = new WWW("http://localhost/sqlconnect/getsbylevel.php", form);
+        yield return request;
+        //Debug.Log(request.text);
+        
+    }
+
+
+
+
+}
