fe0eb1e1df822fb5df1386a2e7b8dc600227779a
unknown <ywroh0619@naver.com>
2020-12-02 04:22:35 +0900

    script manager

--- VR_Programming/Assets/Scripts/ScriptController.cs
+++ VR_Programming/Assets/Scripts/ScriptController.cs
@@ -1,96 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScriptController : MonoBehaviour
 {
-    int step = 0;
-    GameObject True;
-    GameObject False;
-    // Start is called before the first frame update
-    void Start()
-    {
-        True = transform.Find("True").gameObject;
-        False = transform.Find("False").gameObject;
-    }
 
-    // Update is called once per frame
-    void Update()
+    private int nLines = 0;
+    private int maxLines = 12;
+    private int curLine = 1;
+    TextMeshPro ScriptText;
+    TextMeshPro ScriptIdx;
+    string[] script;
+    string[] scriptIndex;
+
+
+    public void SetLine(string[] tscript)
     {
-        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-        switch (step)
+        //find textmeshpro
+        ScriptText = GameObject.Find("ScriptText").gameObject.GetComponent<TextMeshPro>();
+        ScriptIdx = GameObject.Find("ScriptIdx").gameObject.GetComponent<TextMeshPro>();
+
+        nLines = tscript.Length;
+        script = new string[nLines + maxLines];
+        scriptIndex = new string[nLines + maxLines];
+
+        int i = 0;
+        for (; i < maxLines / 2; i++)
         {
-            case 0:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                }
-                break;
-            case 1:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                }
-                break;
-            case 2:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                    True.SetActive(true);
-                    False.SetActive(true);
-                }
-                break;
-            case 3:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                    True.SetActive(false);
-                    False.SetActive(false  );
-
-                }
-                break;
-            case 4:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                }
-                break;
-            case 5:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                }
-                break;
-            case 6:
-                if (Input.GetKeyDown(KeyCode.Tab))
-                {
-                    audioSource.Play();
-                    next();
-                    step++;
-                }
-                break;
+            script[i] = "";
+            scriptIndex[i] = "";
         }
+        for (; i < nLines + maxLines / 2; i++)
+        {
+            scriptIndex[i] = i - maxLines / 2 + 1 + "";
+            script[i] = "| " + tscript[i - maxLines / 2];
+        }
+        for (; i < nLines + maxLines; i++)
+        {
+            script[i] = "";
+            scriptIndex[i] = "";
+        }
+    }
 
-        
-    }
-    public void next()
+    public void ShowScript(int n)
     {
-        Vector3 v = transform.Find("highlight").position;
-        v = new Vector3(v.x, v.y - 0.24f, v.z);
-        transform.Find("highlight").position = v;
+        ScriptIdx.text = "";
+        ScriptText.text = "";
+        if (n + maxLines > nLines + maxLines)
+            n = nLines;
+        for (int i = n - 1; i < n - 1 + maxLines; i++)
+        {
+            ScriptIdx.text += scriptIndex[i] + "\n";
+            ScriptText.text += script[i] + "\n";
+        }
     }
+
 }
 
 
---
+++ VR_Programming/Assets/Scripts/ScriptTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour
+{
+    ScriptController SC = new ScriptController();
+
+    int curLine = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        // change to any string array you want.
+        string[] tscript = new string[12];
+        tscript[0] = "a = input()";
+        tscript[1] = "b = input()";
+        tscript[2] = "c = a + b";
+        tscript[3] = "if c <= 20:";
+        tscript[4] = "    a = a * 7";
+        tscript[5] = "    b = b * 5";
+        tscript[6] = "c = a * b";
+        tscript[7] = "print(c)";
+        tscript[8] = "a = a * 7";
+        tscript[9] = "a = a * 7";
+        tscript[10] = "a = a * 7";
+        tscript[11] = "a = a * 7";
+
+        SC.SetLine(tscript);
+        SC.ShowScript(curLine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            audioSource.Play();
+            SC.ShowScript(++curLine);
+        }
+    }
+}
